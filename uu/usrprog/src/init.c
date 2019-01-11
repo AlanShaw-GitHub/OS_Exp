cb6fb7fb7f9f855267b662b674d3056515222aa7
Alan-Shaw <alanshawzju@gmail.com>
2019-01-11 15:53:10 +0800

    s

--- uu/usrprog/src/init.c
+++ uu/usrprog/src/init.c
@@ -1,29 +1,29 @@
 #include <init.h>
 // #include "myvi.h"
-#include "ls.h"
+// #include "ls.h"
 
 #pragma GCC push_options
 #pragma GCC optimize("O0")
 
 
 unsigned int main(unsigned int argc, char params[][10]) {
-    // printf("we are in the user mode!\n");
-    // printf("begin to malloc a space!\n");
-    // unsigned int ptr = malloc(16);
-    // printf("malloc ptr : %x\n",ptr);
-    // printf("ending!\n");
+    printf("we are in the user mode!\n");
+    printf("begin to malloc a space!\n");
+    unsigned int ptr = malloc(16);
+    printf("malloc ptr : %x\n",ptr);
+    printf("ending!\n");
 
     // unsigned int ret =  myvi(params[1]);
     // unsigned int ret =  myvi("test.txt");
     
     // unsigned int ret = ls((char *)args);
-    unsigned int ret;
-    if (argc > 1)
-        ret = ls(params[1]);
-    else
-        ret = ls("/");
+    // unsigned int ret;
+    // if (argc > 1)
+    //     ret = ls(params[1]);
+    // else
+    //     ret = ls("/");
 
-    return ret;
+    return 0;
 }
 
 #pragma GCC pop_options
